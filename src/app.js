01cea78da2e07082dbda55614e198ca78bbaa855
Michael Kaiser <michaelkaiser@Michaels-MacBook-Pro.local>
2019-03-19 11:26:04 -0700

    Init commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require(`./utils/geocode.js`)
+const forecast = require(`./utils/forecast`)
+
+
+const app = express()
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, `../templates/views`)
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(path.join(publicDirPath)))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App', 
+        name: 'Michael Kaiser'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Michael Kaiser'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is the help page',
+        title: 'Help',
+        name: 'Michael Kaiser'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address.'
+        })
+    }
+    const address = req.query.address
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: address
+            })
+        })
+    })
+
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    res.send({
+        products: []
+    })
+})
+
+app.get(`/help/*`, (req, res) => {
+    res.render('404', {
+        title: '404 Help',
+        name: 'Michael Kaiser',
+        eMessage: 'Help article not found.'
+    })
+})
+app.get(`*`, (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Michael Kaiser',
+        eMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
