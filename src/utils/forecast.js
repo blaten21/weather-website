14ee83a2a202e70e5fb9180b226eb325af999891
Michael Kaiser <michaelkaiser@Michaels-MacBook-Pro.local>
2019-03-19 18:28:21 -0700

    Added more return data from forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,4 @@
 const request = require('request')
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
 
 const forecast = (long, lat, callback) => {
     const url = `https://api.darksky.net/forecast/b1dc02ffad4a46c19987d99894a844e7/${long},${lat}`
@@ -18,7 +8,8 @@ const forecast = (long, lat, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. 
+            The high for today is ${body.daily.data[0].temperatureMax} and the low is ${body.daily.data[0].temperatureMin}`)
         }
     })
 }
